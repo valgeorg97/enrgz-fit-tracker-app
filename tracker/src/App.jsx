3c1a876aa22d8777feb445d238b486556cebeb5f
Valentin Georgiev <valentingeorgiev1997@abv.bg>
2023-05-17 18:40:36 +0300

    install chakra ui, react-router add Home, Profile views

--- tracker/src/App.jsx
+++ tracker/src/App.jsx
@@ -1,14 +1,16 @@
-import './App.css'
+import * as React from "react"
+import { ChakraProvider, Box, Button} from "@chakra-ui/react"
 
 function App() {
-
   return (
-    <>
-      <div>
-       
-      </div>
-    </>
+    <ChakraProvider>
+      <div className="content">
+      <Box>
+      <Button colorScheme="teal">Опа</Button>
+    </Box>
+    </div>
+    </ChakraProvider>
   )
 }
 
-export default App
+export default App;
--- tracker/src/main.jsx
+++ tracker/src/main.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from "react-router-dom";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  
   <React.StrictMode>
+    <ChakraProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>,
+  
 );
