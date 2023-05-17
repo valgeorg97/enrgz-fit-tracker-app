2177a16d0eb9c56c9caccb621d65a7b63120e918
samuilYoshkov <magmatogorsk@protonmail.com>
2023-05-17 16:33:52 +0300

    Project creation

---
+++ tracker/src/context/AuthContext.js
@@ -0,0 +1,27 @@
+import { createContext } from "react";
+
+export const AuthContext = createContext({
+//   isLoggedIn: false,
+//   setIsLoggedIn: () => {},
+//   currentUserEmail: "",
+//   setCurrentUserEmail: () => {},
+//   isAdmin: false,
+//   setIsAdmin: () => {},
+//   signOut: () => {},
+//   email: "",
+//   setEmail: () => {},
+//   password: "",
+//   setPassword: () => {},
+//   name: "",
+//   setName: () => {},
+//   photoURL: "",
+//   setPhotoURL: () => {},
+//   username: "",
+//   setUsername: () => {},
+//   family: "",
+//   setFamily: () => {},
+//   userID: "",
+//   setUserID: () => {},
+//   isBlocked: "",
+//   setIsBlocked: () => {}
+});
---
+++ tracker/src/views/NotFound/NotFound.jsx
@@ -0,0 +1,15 @@
+// Importing required modules
+import './NotFound.css'
+
+/**
+ * NotFound Component
+ *
+ * This component is rendered when the application is navigated to a route that does not exist.
+ *
+ * @returns {JSX.Element} NotFound component
+ */
+const NotFound = () => {
+  return <div>Error 404: Not Found!</div>;
+};
+
+export default NotFound;
