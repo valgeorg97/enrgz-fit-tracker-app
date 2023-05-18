f66b449c26420d8a224061b2698a956fe6c17f73
samuilYoshkov <magmatogorsk@protonmail.com>
2023-05-18 19:40:38 +0300

    Add Profile component style

--- tracker/src/views/Profile/Profile.jsx
+++ tracker/src/views/Profile/Profile.jsx
@@ -1,127 +1,250 @@
-import { useState, useContext } from 'react';
-import { Box, Button, FormControl, FormLabel, Input, VStack,HStack, Heading, Avatar,Text } from '@chakra-ui/react';
-import { AuthContext } from '../../context/AuthContext';
-import { uploadPhoto, auth } from "../../services/firebase"
-import { updateEmail,updateProfile } from "firebase/auth";
+import React from "react";
+import {Container,Avatar,Box,Text,Stack,InputGroup,Input,InputLeftElement,Icon,FormLabel,Checkbox,Link,Button,Divider,FormControl,Heading} from "@chakra-ui/react";
+import { FaRegEnvelope, FaLock, FaRegUser } from "react-icons/fa";
+import PageContainer from "./PageContainer";
+import PageContent from "./PageContent";
+import { useState, useContext } from "react";
+import { updateProfile, updateEmail } from "firebase/auth";
+import { uploadPhoto, auth } from "../../services/firebase";
+import { AuthContext } from "../../context/AuthContext";
 
-const Profile = () => {
-  const { name,setName,email, setEmail, photoURL, userID } = useContext(AuthContext);
+export default function Profile() {
+  const { name, setName, email, setEmail, photoURL, userID } = useContext(AuthContext);
   const [changedName, setChangedName] = useState("");
   const [changedEmail, setChangedEmail] = useState("");
-  const [changedPhoto, setChangedPhoto] = useState(null)
-
-  const [showEmailInputForm, setShowEmailInputForm] = useState(false);
-  function handleShowEmailInputForm() {
-    setShowEmailInputForm((showEmailInputForm) => !showEmailInputForm);
-  }
-  const [showNameInputForm, setShowNameInputForm] = useState(false);
-  function handleShowNameInputForm() {
-    setShowNameInputForm((showNameInputForm) => !showNameInputForm);
-  }
-  const [showAvatarInputForm, setShowAvatarInputForm] = useState(false);
-  function handleShowAvatarInputForm() {
-    setShowAvatarInputForm((showAvatarInputForm) => !showAvatarInputForm);
-  }
-
+  const [changedPhoto, setChangedPhoto] = useState(null);
 
   const handleChangeName = (event) => {
     setChangedName(event.target.value);
   };
-  function changeTheName() {
-    updateProfile(auth.currentUser, {displayName: changedName}).then(() => {
-        setName(changedName)
-    })
-
-  }
-
-
 
   const handleChangeEmail = (event) => {
     setChangedEmail(event.target.value);
   };
-  function changeTheEmail() {
-    updateEmail(auth.currentUser, changedEmail).then(() => {
-      setEmail(changedEmail);
-    });
-  }
-
-
 
   const handleChangeAvatar = (event) => {
     setChangedPhoto(event.target.files[0]);
   };
-  function handleUploadPhoto(changedPhoto) {
+
+  const updateInfo = (event) => {
+    event.preventDefault();
+  
     if (changedPhoto) {
       uploadPhoto(changedPhoto, userID);
     }
-  }
-
-
+    if (changedEmail) {
+      updateEmail(auth.currentUser, changedEmail).then(() => {
+        setEmail(changedEmail);
+      });
+    }
+    if (changedName) {
+      updateProfile(auth.currentUser, { displayName: changedName }).then(() => {
+        setName(changedName);
+      });
+    }
+  };
 
   return (
+    <PageContainer isFixedNav>
+      <PageContent centerContent={true}>
+        <Container maxW="container.sm">
+          <Heading marginBottom="1.5rem">Edit profile</Heading>
+          <form>
 
-<Box p={4}>
-  <VStack spacing={4} align="start">
-    <Heading size="md">Profile</Heading>
+            <Box bg="secondary.card" rounded="lg" p={5}>
+              <Avatar size="2xl" name={name} src={photoURL} />
+              <Stack spacing={4} marginBottom="1rem">
+                <FormControl>
+                  <FormLabel htmlFor="name">Your name</FormLabel>
+                  <InputGroup>
+                    <InputLeftElement
+                      children={
+                        <Icon as={FaRegUser} color="secondary.inputHelper" />
+                      }
+                    />
+                    <Input
+                      focusBorderColor="main.500"
+                      type="text"
+                      name="name"
+                      id="name"
+                      placeholder={name}
+                      onChange={handleChangeName}
+                    />
+                  </InputGroup>
+                </FormControl>
+                <FormControl>
+                  <FormLabel htmlFor="email">Email Address</FormLabel>
+                  <InputGroup>
+                    <InputLeftElement
+                      children={
+                        <Icon
+                          as={FaRegEnvelope}
+                          color="secondary.inputHelper"
+                        />
+                      }
+                    />
+                    <Input
+                      focusBorderColor="main.500"
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder={email}
+                      onChange={handleChangeEmail}
+                    />
+                  </InputGroup>
+                </FormControl>
+              </Stack>
+              <Stack
+                justifyContent="space-between"
+                isInline
+                marginBottom="1rem"
+              >
+                <Stack isInline>
+                  <Checkbox
+                    size="md"
+                    fontWeight="500"
+                    colorScheme="main"
+                    name="subscribe"
+                    id="subscribe"
+                  >
+                    Receive newsletter
+                  </Checkbox>
+                </Stack>
+              </Stack>
+            </Box>
 
-    <Avatar size="2xl" name={name} src={photoURL} />
+            <Stack spacing={0} marginTop="2rem" marginBottom="1rem">
+              <Heading as="h4" size="md">
+                Security settings
+              </Heading>
+              <Text color="gray.500" fontSize="md">
+                Update your password
+              </Text>
+            </Stack>
 
-    <VStack spacing={2} align="start" w="100%">
-      <HStack justify="space-between" w="100%">
-        <Text fontSize="lg">{`Account name: ${name}`}</Text>
-        <Button onClick={handleShowNameInputForm} colorScheme="blue">
-          {showNameInputForm ? "Cancel" : "Change Name"}
-        </Button>
-      </HStack>
-      {showNameInputForm && (
-        <FormControl id="name">
-          <FormLabel>Name</FormLabel>
-          <Input type="text" onChange={handleChangeName} />
-          <Button onClick={changeTheName} colorScheme="blue" alignSelf="flex-end">
-            Update Name
-          </Button>
-        </FormControl>
-      )}
-    </VStack>
+            <Box bg="secondary.card" rounded="lg" p={5}>
+              <Stack spacing={4} marginBottom="1rem">
+                <FormControl>
+                  <Stack justifyContent="space-between" isInline>
+                    <FormLabel htmlFor="old_password">
+                      Current password
+                    </FormLabel>
+                  </Stack>
+                  <InputGroup>
+                    <InputLeftElement
+                      children={
+                        <Icon as={FaLock} color="secondary.inputHelper" />
+                      }
+                    />
+                    <Input
+                      focusBorderColor="main.500"
+                      name="old_password"
+                      id="old_password"
+                      type="password"
+                      placeholder="Enter your current password"
+                    />
+                  </InputGroup>
+                </FormControl>
+                <FormControl>
+                  <Stack justifyContent="space-between" isInline>
+                    <FormLabel htmlFor="new_password">New password</FormLabel>
+                  </Stack>
+                  <InputGroup>
+                    <InputLeftElement
+                      children={
+                        <Icon as={FaLock} color="secondary.inputHelper" />
+                      }
+                    />
+                    <Input
+                      focusBorderColor="main.500"
+                      name="new_password"
+                      id="new_password"
+                      type="password"
+                      placeholder="Enter your new password"
+                    />
+                  </InputGroup>
+                </FormControl>
+                <FormControl>
+                  <Stack justifyContent="space-between" isInline>
+                    <FormLabel htmlFor="new_password2">
+                      Confirm new password
+                    </FormLabel>
+                  </Stack>
+                  <InputGroup>
+                    <InputLeftElement
+                      children={
+                        <Icon as={FaLock} color="secondary.inputHelper" />
+                      }
+                    />
+                    <Input
+                      focusBorderColor="main.500"
+                      name="new_password2"
+                      id="new_password2"
+                      type="password"
+                      placeholder="Confirm your new password"
+                    />
+                  </InputGroup>
+                </FormControl>
+                <FormControl>
+                  <Stack justifyContent="space-between" isInline>
+                    <FormLabel htmlFor="new_avatar">
+                      Upload new avatar
+                    </FormLabel>
+                  </Stack>
+                  <InputGroup>
+                    <InputLeftElement
+                      children={
+                        <Icon as={FaLock} color="secondary.inputHelper" />
+                      }
+                    />
+                    <Input
+                      focusBorderColor="main.500"
+                      type="file"
+                      accept="image/*"
+                      onChange={handleChangeAvatar}
+                    />
+                  </InputGroup>
+                </FormControl>
+              </Stack>
+            </Box>
 
-    <VStack spacing={2} align="start" w="100%">
-      <HStack justify="space-between" w="100%">
-        <Text fontSize="lg">{`Account email: ${email}`}</Text>
-        <Button onClick={handleShowEmailInputForm} colorScheme="blue">
-          {showEmailInputForm ? "Cancel" : "Change Email"}
-        </Button>
-      </HStack>
-      {showEmailInputForm && (
-        <FormControl id="email">
-          <FormLabel>Email</FormLabel>
-          <Input type="email" onChange={handleChangeEmail} />
-          <Button onClick={changeTheEmail} colorScheme="blue" alignSelf="flex-end">
-            Update Email
-          </Button>
-        </FormControl>
-      )}
-    </VStack>
+            <Stack
+              direction={["column", "row"]}
+              spacing="1rem"
+              justify="end"
+              marginTop="2rem"
+            >
+              <Button type="submit" colorScheme="main" variant="outline" onClick={updateInfo}>
+                Update settings
+              </Button>
+              <Button colorScheme="main" variant="outline">
+                Cancel
+              </Button>
+            </Stack>
+          </form>
 
-    <HStack justify="flex-end" w="100%">
-      <Button onClick={handleShowAvatarInputForm} colorScheme="blue">
-        {showAvatarInputForm ? "Cancel" : "Change Avatar"}
-      </Button>
-      {showAvatarInputForm && (
-        <FormControl id="avatar">
-          <FormLabel>Avatar</FormLabel>
-          <Input type="file" onChange={handleChangeAvatar} />
-          {changedPhoto && (
-            <Button onClick={() => handleUploadPhoto(changedPhoto)} colorScheme="blue">
-              Update Avatar
-            </Button>
-          )}
-        </FormControl>
-      )}
-    </HStack>
-  </VStack>
-</Box>
-    
+          <Divider
+            marginTop="2rem"
+            marginBottom="2rem"
+            orientation="horizontal"
+          />
+          <Box bg="secondary.card" rounded="lg" p={5} marginBottom="2rem">
+            <Stack spacing={0} marginBottom="1rem">
+              <Heading as="h4" size="md">
+                Danger zone
+              </Heading>
+              <Text color="gray.500" fontSize="sm">
+                Delete your account and data
+              </Text>
+            </Stack>
+            <Stack spacing={4} marginBottom="1rem">
+              <Button colorScheme="red" variant="outline">
+                Delete your account
+              </Button>
+            </Stack>
+          </Box>
+        </Container>
+      </PageContent>
+    </PageContainer>
   );
-};
-
-export default Profile;
+}
