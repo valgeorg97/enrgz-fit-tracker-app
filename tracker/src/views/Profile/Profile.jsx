59907137ed38a145b0f40a25cfddcf2beaf78bf2
samuilYoshkov <magmatogorsk@protonmail.com>
2023-05-18 14:29:17 +0300

    Add change avatar functionality

--- tracker/src/views/Profile/Profile.jsx
+++ tracker/src/views/Profile/Profile.jsx
@@ -1,48 +1,67 @@
-import { useState } from 'react';
+import { useState,useContext } from 'react';
 import { Box, Button, FormControl, FormLabel, Input, VStack, Heading, Avatar } from '@chakra-ui/react';
+import { AuthContext } from '../../context/AuthContext';
+import {uploadPhoto,auth} from "../../services/firebase"
+import { updateEmail } from "firebase/auth";
+
 
 const Profile = () => {
-  const [user, setUser] = useState({
-    name: 'John Doe',
-    email: 'johndoe@example.com',
-    avatar: 'https://example.com/avatar.jpg',
-  });
+    const { name, setName, email, setEmail, photoURL, setPhotoURL, userID } = useContext(AuthContext);
+    const [changedEmail, setChangedEmail] = useState("");
+    const [showEmailInputForm, setShowEmailInputForm] = useState(false);
+    const [changedPhoto, setChangedPhoto] = useState(null)
+
 
   const handleChangeName = (event) => {
-    setUser((prevUser) => ({ ...prevUser, name: event.target.value }));
+    setName((prevUser) => ({ ...prevUser, name: event.target.value }));
   };
 
   const handleChangeEmail = (event) => {
-    setUser((prevUser) => ({ ...prevUser, email: event.target.value }));
+    setChangedEmail(event)
   };
+  function changeTheEmail() {
+    updateEmail(auth.currentUser, changedEmail).then(() => {
+      setEmail(changedEmail);
+      setShowEmailInputForm(false);
+    });
+  }
+  function handleShowEmailInputForm() {
+    setShowEmailInputForm((showEmailInputForm) => !showEmailInputForm);
+  }
 
   const handleChangeAvatar = (event) => {
-    setUser((prevUser) => ({ ...prevUser, avatar: event.target.value }));
+    setChangedPhoto(event);
   };
+
+  function handleUploadPhoto(changedPhoto) {
+    if (changedPhoto) {
+        uploadPhoto(changedPhoto, userID);
+    }
+  }
 
   return (
     <Box p={4}>
       <VStack spacing={4} align="start">
         <Heading size="md">Profile</Heading>
 
+        <Avatar size="xl" name={name} src={photoURL} />
         <FormControl id="name">
           <FormLabel>Name</FormLabel>
-          <Input type="text" value={user.name} onChange={handleChangeName} />
+          <Input type="text" value={name} onChange={handleChangeName} />
         </FormControl>
 
         <FormControl id="email">
           <FormLabel>Email</FormLabel>
-          <Input type="email" value={user.email} onChange={handleChangeEmail} />
+          <Input type="email" onChange={(e) => handleChangeEmail(e.target.value)} />
+          <Button onClick={() => changeTheEmail()} colorScheme="blue">Update Email</Button>
+
         </FormControl>
 
         <FormControl id="avatar">
           <FormLabel>Avatar</FormLabel>
-          <Input type="text" value={user.avatar} onChange={handleChangeAvatar} />
+          <Input type="file" onChange={(e) => handleChangeAvatar(e.target.files[0])} />
+          <Button onClick={() => handleUploadPhoto(changedPhoto)} colorScheme="blue">Update Avatar</Button>
         </FormControl>
-
-        <Avatar size="xl" name={user.name} src={user.avatar} />
-
-        <Button colorScheme="blue">Save Changes</Button>
       </VStack>
     </Box>
   );
