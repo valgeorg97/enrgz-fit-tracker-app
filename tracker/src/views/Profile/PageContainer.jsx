7a5648eb1a8350405b07bbb17ee100c017d0a761
samuilYoshkov <magmatogorsk@protonmail.com>
2023-05-18 19:40:00 +0300

    Add PagContainer

---
+++ tracker/src/views/Profile/PageContainer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Flex } from "@chakra-ui/react";
+
+
+export default function PageContainer(props) {
+  return (
+    <Flex
+      bg="secondary.background"
+      minHeight="100%"
+      width="100%"
+      alignItems="center"
+      justifyContent="top"
+      flexDirection="column"
+      paddingTop={props.isFixedNav ? { md: "4rem" } : "0"}
+    >
+      {props.children}
+    </Flex>
+  );
+}
