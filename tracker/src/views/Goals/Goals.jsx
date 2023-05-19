b49392262a977657499ca82d683bc07aa05c6848
samuilYoshkov <magmatogorsk@protonmail.com>
2023-05-19 22:44:07 +0300

    Add goals display

--- tracker/src/views/Goals/Goals.jsx
+++ tracker/src/views/Goals/Goals.jsx
@@ -1,59 +1,129 @@
-import { useState,useContext } from "react";
-import {collection,addDoc,serverTimestamp,} from "firebase/firestore";
+import { useState, useContext, useEffect } from "react";
+import { collection, addDoc, serverTimestamp, getDocs, query, where } from "firebase/firestore";
 import { auth, db } from "../../services/firebase";
-import {Box,Button,FormControl,FormLabel,Input,Stack,Text,} from "@chakra-ui/react";
+import { Box, Button, FormControl, FormLabel, Input, Stack, Text,SimpleGrid,Card,CardHeader,Heading,CardBody,CardFooter } from "@chakra-ui/react";
 import { AuthContext } from "../../context/AuthContext";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
 const Goals = () => {
-   const [goalName, setGoalName] = useState("");
-   const [goalFrom, setGoalFrom] = useState("");
-   const [goalTo, setGoalTo] = useState("");
-   const { userID,userDocID } = useContext(AuthContext);
-   const user = auth.currentUser
+  const [goalName, setGoalName] = useState("");
+  const [goalFrom, setGoalFrom] = useState("");
+  const [goalTo, setGoalTo] = useState("");
+  const [goals, setGoals] = useState([]);
 
-   const createGoal = async () => {
-     if (user) {
-       try {
-         const goalDocRef = await addDoc(collection(db, `users/${userDocID}/goals`), {
-           name: goalName,
-           owner: userID,
-           from: goalFrom,
-           to: goalTo,
-           createdAt: serverTimestamp(),
-         });
-         setGoalName("");
-         setGoalFrom("");
-         setGoalTo("");
-         toast.success("Goal created successfully!");
-       } catch (error) {
-         toast.error("Error creating goal:", error);
-       }
-     }
-   };
+  const { userID, userDocID } = useContext(AuthContext);
+  const user = auth.currentUser;
+
+  useEffect(() => {
+    const fetchGoals = async () => {
+      if (userDocID) {
+        try {
+          const q = query(collection(db, `users/${userDocID}/goals`), where("owner", "==", userID));
+          const querySnapshot = await getDocs(q);
+          const goalsData = [];
+          querySnapshot.forEach((doc) => {
+            goalsData.push({ id: doc.id, ...doc.data() });
+          });
+          setGoals(goalsData);
+        } catch (error) {
+          console.error("Error fetching goals:", error);
+        }
+      }
+    };
+
+    fetchGoals();
+  }, [user, userID, userDocID]);
+
+  const createGoal = async () => {
+    if (user) {
+      try {
+        const goalDocRef = await addDoc(collection(db, `users/${userDocID}/goals`), {
+          name: goalName,
+          owner: userID,
+          from: goalFrom,
+          to: goalTo,
+          createdAt: serverTimestamp(),
+        });
+        setGoalName("");
+        setGoalFrom("");
+        setGoalTo("");
+        toast.success("Goal created successfully!");
+      } catch (error) {
+        toast.error("Error creating goal:", error);
+      }
+    }
+  };
 
   return (
-    <Box>
-      <Text mb={4} fontSize="xl" fontWeight="bold">Exercises</Text>
-      <Stack spacing={3}>
-        <FormControl>
-          <FormLabel>Goal Name</FormLabel>
-          <Input type="text" value={goalName} onChange={(e) => setGoalName(e.target.value)}/>
-        </FormControl>
-        <FormControl>
-          <FormLabel>From</FormLabel>
-          <Input type="date" value={goalFrom} onChange={(e) => setGoalFrom(e.target.value)}/>
-        </FormControl>
-        <FormControl>
-          <FormLabel>To</FormLabel>
-          <Input type="date" value={goalTo} onChange={(e) => setGoalTo(e.target.value)}/>
-        </FormControl>
-        <Button colorScheme="teal" onClick={createGoal}>Create Goal</Button>
-      </Stack>
-      <ToastContainer position="top-center" style={{ zIndex: 2001, top: 30 }} />
-    </Box>
+<Box display="flex" flexDirection="column">
+  <Text mb={4} ml={100} fontSize="2xl" fontWeight="bold">
+    Goals
+  </Text>
+  <Box
+    display="flex"
+    flexWrap="wrap"
+    justifyContent="left"
+    mb={3}
+    marginLeft="80px"
+  >
+    {goals.map((goal, index) => (
+      <Box key={index} mr={4} width="240px" height="250px">
+        <Card>
+          <CardHeader>
+            <Heading size="md">{goal.name}</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>
+              <strong>From:</strong> {goal.from}
+            </Text>
+            <Text>
+              <strong>To:</strong> {goal.to}
+            </Text>
+          </CardBody>
+          <CardFooter>
+            <Button>View here</Button>
+          </CardFooter>
+        </Card>
+        </Box>
+    ))}
+  </Box>
+
+  <Box flex="1" marginLeft="20px">
+    <Stack spacing={3} width="200px" height="200px">
+      <FormControl>
+        <FormLabel>Goal Name</FormLabel>
+        <Input
+          type="text"
+          value={goalName}
+          onChange={(e) => setGoalName(e.target.value)}
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel>From</FormLabel>
+        <Input
+          type="date"
+          value={goalFrom}
+          onChange={(e) => setGoalFrom(e.target.value)}
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel>To</FormLabel>
+        <Input
+          type="date"
+          value={goalTo}
+          onChange={(e) => setGoalTo(e.target.value)}
+        />
+      </FormControl>
+      <Button colorScheme="teal" onClick={createGoal}>
+        Create Goal
+      </Button>
+    </Stack>
+  </Box>
+  <ToastContainer position="top-center" style={{ zIndex: 2001, top: 30 }} />
+</Box>
+
+
   );
 };
 
--- tracker/src/views/Profile/Profile.jsx
+++ tracker/src/views/Profile/Profile.jsx
@@ -1,4 +1,5 @@
-import {Container,Avatar,Box,Text,Stack,InputGroup,Input,InputLeftElement,Icon,FormLabel,Checkbox,Link,Button,Divider,FormControl,Heading} from "@chakra-ui/react";
+/* eslint-disable react/no-children-prop */
+import {Container,Avatar,Box,Text,Stack,InputGroup,Input,InputLeftElement,Icon,FormLabel,Checkbox,Button,Divider,FormControl,Heading,} from "@chakra-ui/react";
 import { FaRegEnvelope, FaLock, FaRegUser } from "react-icons/fa";
 import PageContainer from "./PageContainer";
 import PageContent from "./PageContent";
@@ -6,16 +7,21 @@ import { useState, useContext, useRef } from "react";
 import { updateProfile, updateEmail } from "firebase/auth";
 import { uploadPhoto, auth, db } from "../../services/firebase";
 import { AuthContext } from "../../context/AuthContext";
-import { updateDoc,doc } from "firebase/firestore";
+import { updateDoc, doc } from "firebase/firestore";
 import { toast } from "react-toastify";
 
 export default function Profile() {
-  const { name, setName, email, setEmail, photoURL, setPhotoURL, userID, family,setFamily,userDocID } = useContext(AuthContext);
+  const {name,setName,email,setEmail,photoURL,setPhotoURL,userID,family,setFamily,userDocID,} = useContext(AuthContext);
   const [changedName, setChangedName] = useState("");
   const [changedFamily, setChangedFamily] = useState("");
   const [changedEmail, setChangedEmail] = useState("");
   const [changedPhoto, setChangedPhoto] = useState(null);
-  const avatarInputRef = useRef(null)
+
+  const avatarInputRef = useRef(null);
+  const nameInputRef = useRef(null);
+  const familyInputRef = useRef(null);
+  const emailInputRef = useRef(null);
+  const passwordInputRef = useRef(null);
 
   const handleChangeName = (event) => {
     setChangedName(event.target.value);
@@ -29,6 +35,13 @@ export default function Profile() {
   const handleChangeAvatar = (event) => {
     setChangedPhoto(event.target.files[0]);
   };
+  const handleCancel = () => {
+    avatarInputRef.current.value = null;
+    nameInputRef.current.value = null;
+    familyInputRef.current.value = null;
+    emailInputRef.current.value = null;
+    passwordInputRef.current.value = null;
+  };
 
   const updateInfo = (event) => {
     event.preventDefault();
@@ -39,31 +52,28 @@ export default function Profile() {
     }
 
     if (changedPhoto) {
-      uploadPhoto(changedPhoto, userID)
-      .then(() => {
-        setPhotoURL(changedPhoto)
-        avatarInputRef.current.value = null
-      })
-
+      uploadPhoto(changedPhoto, userID).then(() => {
+        setPhotoURL(changedPhoto);
+        avatarInputRef.current.value = null;
+      });
     }
     if (changedEmail) {
-      updateEmail(auth.currentUser, changedEmail)
-      .then(() => {
+      updateEmail(auth.currentUser, changedEmail).then(() => {
         const userRef = doc(db, "users", userDocID);
-          updateDoc(userRef, { email: changedEmail })
-            .then(() => {
-              setEmail(changedEmail);
-            })
-            .catch((error) => {
-              console.log("Error updating email:", error);
-            });
-        })
+        updateDoc(userRef, { email: changedEmail })
+          .then(() => {
+            setEmail(changedEmail);
+          })
+          .catch((error) => {
+            console.log("Error updating email:", error);
+          });
+      });
     }
     if (changedName) {
-      let fixname = `${changedName} ${family}`
+      let fixname = `${changedName} ${family}`;
       updateProfile(auth.currentUser, { displayName: fixname })
-      .then(() => {
-        const userRef = doc(db, "users", userDocID);
+        .then(() => {
+          const userRef = doc(db, "users", userDocID);
           updateDoc(userRef, { name: changedName })
             .then(() => {
               setName(changedName);
@@ -77,7 +87,7 @@ export default function Profile() {
         });
     }
     if (changedFamily) {
-      let fixfamily = `${name} ${changedFamily}`
+      let fixfamily = `${name} ${changedFamily}`;
       updateProfile(auth.currentUser, { displayName: fixfamily })
         .then(() => {
           const userRef = doc(db, "users", userDocID);
@@ -100,202 +110,168 @@ export default function Profile() {
       <PageContent centerContent={true}>
         <Container maxW="container.sm">
           <Heading marginBottom="1.5rem">Edit profile</Heading>
-          <form>
 
-            <Box bg="secondary.card" rounded="lg" p={5}>
-              <Avatar size="2xl" name={name} src={photoURL} />
-              <Stack spacing={4} marginBottom="1rem">
-                <FormControl>
-                  <FormLabel htmlFor="name">Your first name</FormLabel>
-                  <InputGroup>
-                    <InputLeftElement
-                      children={
-                        <Icon as={FaRegUser} color="secondary.inputHelper" />
-                      }
-                    />
-                    <Input
-                      focusBorderColor="main.500"
-                      type="text"
-                      name="name"
-                      id="name"
-                      placeholder={name}
-                      onChange={handleChangeName}
-                    />
-                  </InputGroup>
-                </FormControl>
+          <Box bg="secondary.card" rounded="lg" p={5}>
+            <Avatar size="2xl" name={name} src={photoURL} />
+            <Stack spacing={4} marginBottom="1rem">
+              <FormControl>
+                <FormLabel htmlFor="name">Your first name</FormLabel>
+                <InputGroup>
+                  <InputLeftElement children={ <Icon as={FaRegUser} color="secondary.inputHelper" />}/>
+                  <Input
+                    focusBorderColor="main.500"
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder={name}
+                    onChange={handleChangeName}
+                    ref={nameInputRef}
+                  />
+                </InputGroup>
+              </FormControl>
 
-                <FormControl>
-                  <FormLabel htmlFor="family">Your family name</FormLabel>
-                  <InputGroup>
-                    <InputLeftElement
-                      children={
-                        <Icon as={FaRegUser} color="secondary.inputHelper" />
-                      }
-                    />
-                    <Input
-                      focusBorderColor="main.500"
-                      type="text"
-                      name="family"
-                      id="family"
-                      placeholder={family}
-                      onChange={handleChangeFamily}
-                    />
-                  </InputGroup>
-                </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="family">Your family name</FormLabel>
+                <InputGroup>
+                  <InputLeftElement  children={<Icon as={FaRegUser} color="secondary.inputHelper" />}/>
+                  <Input
+                    focusBorderColor="main.500"
+                    type="text"
+                    name="family"
+                    id="family"
+                    placeholder={family}
+                    onChange={handleChangeFamily}
+                    ref={familyInputRef}
+                  />
+                </InputGroup>
+              </FormControl>
 
-
-                <FormControl>
-                  <FormLabel htmlFor="email">Email Address</FormLabel>
-                  <InputGroup>
-                    <InputLeftElement
-                      children={
-                        <Icon
-                          as={FaRegEnvelope}
-                          color="secondary.inputHelper"
-                        />
-                      }
-                    />
-                    <Input
-                      focusBorderColor="main.500"
-                      type="email"
-                      name="email"
-                      id="email"
-                      placeholder={email}
-                      onChange={handleChangeEmail}
-                    />
-                  </InputGroup>
-                </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="email">Email Address</FormLabel>
+                <InputGroup>
+                  <InputLeftElement children={<Icon as={FaRegEnvelope} color="secondary.inputHelper" />}/>
+                  <Input
+                    focusBorderColor="main.500"
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder={email}
+                    onChange={handleChangeEmail}
+                    ref={emailInputRef}
+                  />
+                </InputGroup>
+              </FormControl>
+            </Stack>
+            <Stack justifyContent="space-between" isInline marginBottom="1rem">
+              <Stack isInline>
+                <Checkbox
+                  size="md"
+                  fontWeight="500"
+                  colorScheme="main"
+                  name="subscribe"
+                  id="subscribe"
+                >
+                  Receive newsletter
+                </Checkbox>
               </Stack>
-              <Stack
-                justifyContent="space-between"
-                isInline
-                marginBottom="1rem"
-              >
-                <Stack isInline>
-                  <Checkbox
-                    size="md"
-                    fontWeight="500"
-                    colorScheme="main"
-                    name="subscribe"
-                    id="subscribe"
-                  >
-                    Receive newsletter
-                  </Checkbox>
+            </Stack>
+          </Box>
+
+          <Stack spacing={0} marginTop="2rem" marginBottom="1rem">
+            <Heading as="h4" size="md">
+              Security settings
+            </Heading>
+            <Text color="gray.500" fontSize="md">
+              Update your password
+            </Text>
+          </Stack>
+
+          <Box bg="secondary.card" rounded="lg" p={5}>
+            <Stack spacing={4} marginBottom="1rem">
+              <FormControl>
+                <Stack justifyContent="space-between" isInline>
+                  <FormLabel htmlFor="old_password">Current password</FormLabel>
                 </Stack>
-              </Stack>
-            </Box>
-
-            <Stack spacing={0} marginTop="2rem" marginBottom="1rem">
-              <Heading as="h4" size="md">
-                Security settings
-              </Heading>
-              <Text color="gray.500" fontSize="md">
-                Update your password
-              </Text>
+                <InputGroup>
+                  <InputLeftElement children={ <Icon as={FaLock} color="secondary.inputHelper" />}/>
+                  <Input
+                    focusBorderColor="main.500"
+                    name="old_password"
+                    id="old_password"
+                    type="password"
+                    placeholder="Enter your current password"
+                  />
+                </InputGroup>
+              </FormControl>
+              <FormControl>
+                <Stack justifyContent="space-between" isInline>
+                  <FormLabel htmlFor="new_password">New password</FormLabel>
+                </Stack>
+                <InputGroup>
+                  <InputLeftElement children={ <Icon as={FaLock} color="secondary.inputHelper" />}/>
+                  <Input
+                    focusBorderColor="main.500"
+                    name="new_password"
+                    id="new_password"
+                    type="password"
+                    placeholder="Enter your new password"
+                    ref={passwordInputRef}
+                  />
+                </InputGroup>
+              </FormControl>
+              <FormControl>
+                <Stack justifyContent="space-between" isInline>
+                  <FormLabel htmlFor="new_password2">
+                    Confirm new password
+                  </FormLabel>
+                </Stack>
+                <InputGroup>
+                  <InputLeftElement children={ <Icon as={FaLock} color="secondary.inputHelper" /> }/>
+                  <Input
+                    focusBorderColor="main.500"
+                    name="new_password2"
+                    id="new_password2"
+                    type="password"
+                    placeholder="Confirm your new password"
+                  />
+                </InputGroup>
+              </FormControl>
+              <FormControl>
+                <Stack justifyContent="space-between" isInline>
+                  <FormLabel htmlFor="new_avatar">Upload new avatar</FormLabel>
+                </Stack>
+                <InputGroup>
+                  <InputLeftElement children={ <Icon as={FaLock} color="secondary.inputHelper" /> }/>
+                  <Input
+                    focusBorderColor="main.500"
+                    type="file"
+                    accept="image/*"
+                    onChange={handleChangeAvatar}
+                    ref={avatarInputRef}
+                  />
+                </InputGroup>
+              </FormControl>
             </Stack>
+          </Box>
 
-            <Box bg="secondary.card" rounded="lg" p={5}>
-              <Stack spacing={4} marginBottom="1rem">
-                <FormControl>
-                  <Stack justifyContent="space-between" isInline>
-                    <FormLabel htmlFor="old_password">
-                      Current password
-                    </FormLabel>
-                  </Stack>
-                  <InputGroup>
-                    <InputLeftElement
-                      children={
-                        <Icon as={FaLock} color="secondary.inputHelper" />
-                      }
-                    />
-                    <Input
-                      focusBorderColor="main.500"
-                      name="old_password"
-                      id="old_password"
-                      type="password"
-                      placeholder="Enter your current password"
-                    />
-                  </InputGroup>
-                </FormControl>
-                <FormControl>
-                  <Stack justifyContent="space-between" isInline>
-                    <FormLabel htmlFor="new_password">New password</FormLabel>
-                  </Stack>
-                  <InputGroup>
-                    <InputLeftElement
-                      children={
-                        <Icon as={FaLock} color="secondary.inputHelper" />
-                      }
-                    />
-                    <Input
-                      focusBorderColor="main.500"
-                      name="new_password"
-                      id="new_password"
-                      type="password"
-                      placeholder="Enter your new password"
-                    />
-                  </InputGroup>
-                </FormControl>
-                <FormControl>
-                  <Stack justifyContent="space-between" isInline>
-                    <FormLabel htmlFor="new_password2">
-                      Confirm new password
-                    </FormLabel>
-                  </Stack>
-                  <InputGroup>
-                    <InputLeftElement
-                      children={
-                        <Icon as={FaLock} color="secondary.inputHelper" />
-                      }
-                    />
-                    <Input
-                      focusBorderColor="main.500"
-                      name="new_password2"
-                      id="new_password2"
-                      type="password"
-                      placeholder="Confirm your new password"
-                    />
-                  </InputGroup>
-                </FormControl>
-                <FormControl>
-                  <Stack justifyContent="space-between" isInline>
-                    <FormLabel htmlFor="new_avatar">
-                      Upload new avatar
-                    </FormLabel>
-                  </Stack>
-                  <InputGroup>
-                    <InputLeftElement
-                      children={
-                        <Icon as={FaLock} color="secondary.inputHelper" />
-                      }
-                    />
-                    <Input
-                      focusBorderColor="main.500"
-                      type="file"
-                      accept="image/*"
-                      onChange={handleChangeAvatar}
-                      ref={avatarInputRef}
-                    />
-                  </InputGroup>
-                </FormControl>
-              </Stack>
-            </Box>
-
-            <Stack
-              direction={["column", "row"]}
-              spacing="1rem"
-              justify="end"
-              marginTop="2rem"
+          <Stack
+            direction={["column", "row"]}
+            spacing="1rem"
+            justify="end"
+            marginTop="2rem"
+          >
+            <Button
+              type="submit"
+              colorScheme="main"
+              variant="outline"
+              onClick={updateInfo}
             >
-              <Button type="submit" colorScheme="main" variant="outline" onClick={updateInfo}>
-                Update settings
-              </Button>
-              <Button colorScheme="main" variant="outline">
-                Cancel
-              </Button>
-            </Stack>
-          </form>
-
+              Update settings
+            </Button>
+            <Button colorScheme="main" variant="outline" onClick={handleCancel}>
+              Cancel
+            </Button>
+          </Stack>
           <Divider
             marginTop="2rem"
             marginBottom="2rem"
