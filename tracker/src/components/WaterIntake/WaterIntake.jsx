c4d8b3876191fb5fd0bd651f57a8ac1cf4dd9366
Valentin Georgiev <valentingeorgiev1997@abv.bg>
2023-05-23 16:23:09 +0300

    Add Water Intake Calculator to Firestore

--- tracker/src/components/WaterIntake/WaterIntake.jsx
+++ tracker/src/components/WaterIntake/WaterIntake.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { db } from '../../services/firebase';
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { AuthContext } from '../../context/AuthContext';
-import { Box, Input, Heading, Progress, Text, VStack } from "@chakra-ui/react";
+import { Box, Input, Heading, Progress, Text, VStack, Button } from "@chakra-ui/react";
 
 const WaterCalculator = () => {
   const [weight, setWeight] = useState(null);
   const [consumedWater, setConsumedWater] = useState(0);
+  const [savedWater, setSavedWater] = useState(0);
   const { userID, userDocID } = useContext(AuthContext);
 
   useEffect(() => {
@@ -17,6 +18,16 @@ const WaterCalculator = () => {
 
         if (docSnap.exists()) {
           setWeight(docSnap.data().weight);
+
+          const lastUpdate = docSnap.data().lastUpdate?.toDate();
+          const today = new Date();
+          today.setHours(0, 0, 0, 0);  // remove time part
+
+          if (!lastUpdate || lastUpdate.getTime() !== today.getTime()) {
+            setSavedWater(0); // reset water consumed
+          } else {
+            setSavedWater(docSnap.data().consumedWater || 0);
+          }
         } else {
           console.log("No such document!");
         }
@@ -28,44 +39,76 @@ const WaterCalculator = () => {
 
   const calculateWaterIntake = () => {
     if (!weight) {
-      return 'Loading...';
+      return 0; 
     }
-
+  
     let waterIntake = 0.03 * weight;
     return waterIntake;
   }
 
   const calculatePercentage = () => {
     let totalIntake = calculateWaterIntake();
-    let percentage = (consumedWater / totalIntake) * 100;
-    return Math.min(percentage, 100);  // cap it at 100%
+  
+    if (totalIntake === 0 || isNaN(savedWater)) {
+      return 0;
+    }
+  
+    let percentage = (savedWater / totalIntake) * 100;
+    percentage = Math.min(percentage, 100);  
+  
+    return Math.floor(percentage);  
+  }
+
+  const saveWaterIntake = async () => {
+    if (userDocID) {
+      const userRef = doc(db, 'users', userDocID);
+      const today = new Date();
+      today.setHours(0, 0, 0, 0);  
+      await setDoc(userRef, { consumedWater: savedWater + consumedWater, lastUpdate: today }, { merge: true });
+      setSavedWater(savedWater + consumedWater);
+      setConsumedWater(0); 
+    }
   }
 
   return (
-    <VStack spacing={5} width="sm">
-      <Heading>Water Intake</Heading>
-      <Box>
-        <Text>Your weight: {weight ? weight + ' kg' : 'Loading...'}</Text>
-      </Box>
-      <Box>
-        <Text>Your recommended water intake is: {calculateWaterIntake()} liters</Text>
-      </Box>
-      <Box>
-        <Text>Water consumed today (in liters): </Text>
-        <Input 
-          type="number" 
-          value={consumedWater}
-          min="0"
-          onChange={(e) => setConsumedWater(parseFloat(e.target.value))}
-        />
-      </Box>
-      <Box>
-        <Text>Progress:</Text>
-        <Progress value={calculatePercentage()} max="100" />
-        <Text>{calculatePercentage()}%</Text>
-      </Box>
-    </VStack>
+    <Box shadow="lg" p={2} bg="white" borderRadius="md" maxW="md" mx="auto" my={8}>
+      <VStack spacing={4} width="sm">
+        <Heading size="md">Water Intake</Heading>
+        <Box>
+          <Text fontSize="sm">Your weight: {weight ? weight + ' kg' : 'Loading...'}</Text>
+        </Box>
+        <Box>
+          <Text fontSize="sm">Your recommended water intake is: {calculateWaterIntake()} liters</Text>
+        </Box>
+        <Box>
+          <Text fontSize="sm">Add water consumed today (in liters): </Text>
+          <Input 
+            size="sm"
+            type="number" 
+            value={consumedWater}
+            min="0"
+            onChange={(e) => setConsumedWater(parseFloat(e.target.value))}
+          />
+        </Box>
+        <Box>
+          <Button size="sm" onClick={saveWaterIntake}>Save Water Intake</Button>
+        </Box>
+        <Box>
+          <Text fontSize="sm">Your water intake today: {savedWater} liters</Text>
+        </Box>
+        <Box>
+          <Text fontSize="sm">Progress:</Text>
+          <Progress size="sm" value={calculatePercentage()} max="100" />
+          <Text fontSize="sm">{calculatePercentage()}%</Text>
+        </Box>
+        <Box>
+          {calculatePercentage() === 100 ? <Text fontSize="sm" color="green.500">You have reached your goal!</Text> : null}
+        </Box>
+      </VStack>
+    </Box>
   );
+  
+  
 }
 
 export default WaterCalculator;
