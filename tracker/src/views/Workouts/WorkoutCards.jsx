3e6ed8c8f428d32e3964240805aade214b105b55
samuilYoshkov <magmatogorsk@protonmail.com>
2023-05-24 23:06:49 +0300

    Add chakraUI to Workouts+Goals

--- tracker/src/views/Workouts/WorkoutCards.jsx
+++ tracker/src/views/Workouts/WorkoutCards.jsx
@@ -1,88 +1,57 @@
-import { Box, Heading, Button, VStack, Badge, Text, Flex } from "@chakra-ui/react";
-import { FaTrashAlt } from "react-icons/fa";
+import { Box, Heading, Button,Card as ChakraCard,CardHeader,CardFooter, Badge, Text, Flex } from "@chakra-ui/react";
 import { RxEyeOpen } from "react-icons/rx";
 import { FiShare2 } from "react-icons/fi";
 
-const WorkoutCards = ({
-  shared,
-  workouts,
-  handleDeleteWorkout,
-  handleViewMoreClick,
-  handleShareWorkout,
-  difficultyColors
-}) => {
+const WorkoutCards = ({shared,workouts,handleViewMoreClick,handleShareWorkout,difficultyColors}) => {
   return (
-    <VStack spacing={4} mt={5}>
-      {workouts.map((workout, index) => (
-        <Box
+    <Flex flexWrap="wrap" justifyContent="flex-start" mt={5} ml={-4}>
+      {workouts.map((workout, index) => {
+        if (!workout.name) return null;
+        return (
+          <Box
           key={index}
-          p={5}
-          boxShadow="md"
-          borderWidth="1px"
-          borderRadius="lg"
-          backgroundColor="white"
+          mr={4}
+          mb={5}
+          width="290px"
+          height="250px"
+          display="flex"
+          flexDirection="column"
+          justifyContent="space-between"
         >
-          <Flex justify="space-between">
-            <Heading as="h2" size="md" mb={3}>
-              {workout.name}
-            </Heading>
-
-            {!shared && (
-                <Button
-              size="xs"
-              colorScheme="red"
-              onClick={() => handleDeleteWorkout(workout.id)}
-            >
-              <Flex align="center">
-                <FaTrashAlt />
-              </Flex>
-            </Button>
-            )}
-          </Flex>
-
-          <Box>
-            <Text>
-              <strong>Muscle Group:</strong> {workout.muscle}
-            </Text>
-            <Text>
-              <strong>Number of Exercises:</strong>{" "}
-              {workout.exercises ? workout.exercises.length : 0}
-            </Text>
-            <Text>
-              <strong>Difficulty:</strong>{" "}
-              <Badge colorScheme={difficultyColors[workout.difficulty]}>
-                {workout.difficulty}
-              </Badge>
-            </Text>
-            <Button
-              float="right"
-              size="sm"
-              colorScheme="green"
-              onClick={() => {
-                handleViewMoreClick(workout);
-              }}
-            >
-              <Flex align="center">
-                <RxEyeOpen />
-              </Flex>
-            </Button>
-            {!shared && (
-                <Button
-                float="right"
-                size="sm"
-                colorScheme="blue"
-                onClick={() => handleShareWorkout(workout.id)}
-              >
-                <Flex align="center">
-                  <FiShare2 />
-                </Flex>
+          <ChakraCard
+            background="linear-gradient(140deg, #0093E9, #80D0C7)"
+            boxShadow="dark-lg"
+            rounded="md"
+            borderColor="gray.50"
+            height="100%"
+            p={4}
+          >
+            <CardHeader height="160px">
+              <Heading as="h2" size="md" mb={3}>{workout.name}</Heading>
+              <Text><strong>Muscle Group:</strong> {workout.muscle}</Text>
+              <Text><strong>Number of Exercises:</strong>{" "}{workout.exercises ? workout.exercises.length : 0}</Text>
+              <Text>
+                <strong>Difficulty:</strong>{" "}
+                <Badge colorScheme={difficultyColors[workout.difficulty]}>
+                  {workout.difficulty}
+                </Badge>
+              </Text>
+            </CardHeader>
+            <CardFooter justifyContent="right">
+              {!shared && (
+                <Button mr="5px" float="right" size="sm" colorScheme="blue" onClick={() => handleShareWorkout(workout.id)}>
+                  <Flex align="center"><FiShare2 /></Flex>
+                </Button>
+              )}
+              <Button float="right" size="sm" colorScheme="green" onClick={() => {handleViewMoreClick(workout)}}>
+                <Flex align="center"><RxEyeOpen /></Flex>
               </Button>
-            )}
-          </Box>
+            </CardFooter>
+          </ChakraCard>
         </Box>
-      ))}
-    </VStack>
+      )
+    })}
+    </Flex>
   );
-};
-
-export default WorkoutCards;
+}
+export default WorkoutCards
