732671fa57cde1a59315a49e502060cbb99fa713
samuilYoshkov <magmatogorsk@protonmail.com>
2023-06-03 12:17:29 +0300

    Fix Friends not displaying content

--- tracker/src/views/Friends/Friends.jsx
+++ tracker/src/views/Friends/Friends.jsx
@@ -1,4 +1,4 @@
-import {Box,Text,Button,ButtonGroup,Heading,Flex,} from "@chakra-ui/react";
+import {Box,Text,Button,ButtonGroup,Flex,Heading,} from "@chakra-ui/react";
 import { AuthContext } from "../../context/AuthContext";
 import { useContext,useRef } from "react";
 import {doc,updateDoc,getDoc,} from "firebase/firestore";
@@ -8,6 +8,7 @@ import goalheader from "../../assets/goal.png"
 import {BsFillPersonXFill,} from "react-icons/bs";
 import { FriendsContext } from "../../context/FriendsContext";
 import "./Friends.css"
+import user2 from "../../assets/user2.png"
 
 const Friends = () => {
   const { userDocID } = useContext(AuthContext);
@@ -65,29 +66,27 @@ const Friends = () => {
       };
   
       await updateDoc(friendDocRef, updatedFriendData);
+      const updatedUserDoc = await getDoc(userDocRef);
+      const updatedUserData = updatedUserDoc.data();
   
-      // Update the local state immediately
-      setRequests(updatedRequests);
-      setFriends([...friends, request]);
+      const requestsData = updatedUserData?.requests || [];
+      setRequests(requestsData);
+  
+      const friendsData = updatedUserData?.friends || [];
+      const filteredFriends = [];
+  
+      for (const friend of friendsData) {
+        const friendDocRef = doc(db, "users", friend.userDocID);
+        const friendDoc = await getDoc(friendDocRef);
+        const friendData = friendDoc.data();
+        filteredFriends.push(friendData);
+      }
+      setFriends(filteredFriends);
     } catch (error) {
       console.log("Error accepting request:", error);
     }
   };
   
-
-  const handleDecline = async (request) => {
-    try {
-      const userDocRef = doc(db, "users", userDocID);
-      await updateDoc(userDocRef, {
-        requests: requests.filter((req) => req.userDocID !== request.userDocID),
-      });
-  
-      // Update the local state immediately
-      setRequests(requests.filter((req) => req.userDocID !== request.userDocID));
-    } catch (error) {
-      console.log("Error declining request:", error);
-    }
-  };
   
   const handleRemoveFriend = async (friendId) => {
     try {
@@ -110,8 +109,6 @@ const Friends = () => {
       );
   
       await updateDoc(friendDocRef, { friends: updatedFriendFriends });
-  
-      // Update the local state immediately
       setFriends(updatedFriends);
     } catch (error) {
       console.log("Error removing friend:", error);
@@ -120,14 +117,14 @@ const Friends = () => {
   
   
   return (
-    <Box w="1660px" ml="54px">
-      <Grid gap={4} templateRows="repeat(3, 1fr)" templateColumns="repeat(5, 1fr)"  h="600px">
+<Box w="1660px" ml="54px">
+  <Grid gap={4} templateRows="repeat(3, 1fr)" templateColumns="repeat(5, 1fr)" h="600px">
 
-      <GridItem colSpan={5} rounded="md" borderColor="gray.50" h="140px" w="1600px" bgImage={goalheader} p={8}/>
+    <GridItem colSpan={5} rounded="md" borderColor="gray.50" h="140px" w="1600px" bgImage={goalheader} p={8}/>
 
-      <GridItem colSpan={4}>
+    <GridItem colSpan={4}>
       <Text ml={2} mb={4} fontSize="2xl" fontWeight="bold">Friends</Text>
-        <Table variant="simple">
+        <Table key={friends.userDocID} variant="simple">
           <Thead>
             <Tr>
               <Th>Name</Th>
@@ -136,16 +133,17 @@ const Friends = () => {
               <Th>Phone</Th>
             </Tr>
           </Thead>
+      {friends.length>0 ? (
           <Tbody>
             {friends.map((user) => (
               <Tr key={user.docID}>
                 <Td>
                   <Flex align="center">
-                    <Avatar
-                      size="sm"
-                      src={user.avatar}
-                      _hover={{ cursor: "pointer" }}
-                    />
+                  <Avatar
+                    size="sm"
+                    src={user.avatar ? user.avatar : user2}
+                    _hover={{ cursor: "pointer" }}
+                  />
                     <Text ml="2">
                       {user.name} {user.family}
                     </Text>
@@ -154,7 +152,6 @@ const Friends = () => {
                 <Td>{user.username}</Td>
                 <Td>{user.email}</Td>
                 <Td>{user.phoneNumber}</Td>
-
                 <Td>
                   <Tooltip label="Remove from friends">
                     <Button
@@ -174,48 +171,49 @@ const Friends = () => {
               </Tr>
             ))}
           </Tbody>
+      ) : (
+        <Heading mt={10}>Your friend list is empty.</Heading>
+      )}
         </Table>
-      </GridItem>
+    </GridItem>
 
-      <GridItem colSpan={1}>
-        <Text ml={2} mb={4} fontSize="2xl" fontWeight="bold">Friend requests</Text>
-        {requests.map((request) => (
-          <Popover
-            key={request.id}
-            initialFocusRef={initialFocusRef}
-            placement="bottom"
-            closeOnBlur={false}
-            
-          >
-            <PopoverTrigger>
-              <Button bgColor="#e84a5f" mt={3} className="reqbutton">{request.name} wants to be friends.</Button>
-            </PopoverTrigger>
-            <PopoverContent color="white" bg="blue.800" borderColor="blue.800">
-              <PopoverHeader pt={4} fontWeight="bold" border="0">
-                {request.name} {request.family}
-              </PopoverHeader>
-              <PopoverArrow />
-              <PopoverCloseButton />
-              <PopoverBody>Hello, can we be friends ?</PopoverBody>
+    <GridItem colSpan={1}>
+      <Text ml={2} mb={4} fontSize="2xl" fontWeight="bold">Friend requests</Text>
+      {requests.map((request) => (
+        <Popover
+          key={request.userDocID}
+          initialFocusRef={initialFocusRef}
+          placement="bottom"
+          closeOnBlur={false}
+        >
+          <PopoverTrigger>
+            <Button bgColor="#e84a5f" mt={3} className="reqbutton">{request.name} wants to be friends.</Button>
+          </PopoverTrigger>
+          <PopoverContent color="white" bg="blue.800" borderColor="blue.800">
+            <PopoverHeader pt={4} fontWeight="bold" border="0">
+              {request.name} {request.family}
+            </PopoverHeader>
+            <PopoverArrow />
+            <PopoverCloseButton />
+            <PopoverBody>Hello, can we be friends ?</PopoverBody>
+            <PopoverFooter border="0" display="flex" alignItems="center" justifyContent="space-between" pb={4}>
+              <ButtonGroup size="sm">
+                <Button onClick={() => handleAccept(request)} ref={initialFocusRef} colorScheme="whatsapp">
+                  Accept
+                </Button>
+                <Button onClick={() => handleDecline(request)} colorScheme="red">
+                  Decline
+                </Button>
+              </ButtonGroup>
+            </PopoverFooter>
+          </PopoverContent>
+        </Popover>
+      ))}
+    </GridItem>
 
-              <PopoverFooter border="0" display="flex" alignItems="center" justifyContent="space-between" pb={4}>
-                <ButtonGroup size="sm">
-                  <Button onClick={() => handleAccept(request)} ref={initialFocusRef} colorScheme="whatsapp">
-                    Accept
-                  </Button>
-                  <Button onClick={() => handleDecline(request)} colorScheme="red">
-                    Decline
-                  </Button>
-                </ButtonGroup>
-              </PopoverFooter>
+  </Grid>
+</Box>
 
-            </PopoverContent>
-          </Popover>
-        ))}
-      </GridItem>
-
-      </Grid>
-    </Box>
   );
 };
 
