a45a02f0ab7e8f49fe7b41c8d46bdcb6025b6ed5
samuilYoshkov <magmatogorsk@protonmail.com>
2023-06-03 10:53:59 +0300

    Fix Community page styling

--- tracker/src/views/Community/Community.jsx
+++ tracker/src/views/Community/Community.jsx
@@ -107,7 +107,7 @@ const Community = () => {
   };
 
   return (
-<Box w="1660px" ml="54px">
+<Box w="1560px">
       <Grid templateColumns="repeat(1, 1fr)"  h="600px">
 
         <GridItem colSpan={1} rounded="md" borderColor="gray.50" h="140px" w="1600px" bgImage={goalheader} p={8}/>
