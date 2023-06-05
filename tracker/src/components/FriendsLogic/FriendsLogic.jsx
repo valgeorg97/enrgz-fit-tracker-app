f16ffa72f7aa41ae36478cd1f68e2697f0c319aa
samuilYoshkov <magmatogorsk@protonmail.com>
2023-06-05 13:38:20 +0300

    Add FriendsLogic to Friends component

---
+++ tracker/src/components/FriendsLogic/FriendsLogic.jsx
@@ -0,0 +1,117 @@
+import { AuthContext } from "../../context/AuthContext";
+import { useContext,useRef } from "react";
+import {doc,updateDoc,getDoc,} from "firebase/firestore";
+import { db } from "../../config/firebase";
+import { FriendsContext } from "../../context/FriendsContext";
+
+const FriendsLogic = () => {
+  const { userDocID } = useContext(AuthContext);
+  const {requests, setRequests, setFriends} = useContext(FriendsContext)
+  const initialFocusRef = useRef();
+
+  const handleAccept = async (request) => {
+    try {
+      const userDocRef = doc(db, "users", userDocID);
+      const userDoc = await getDoc(userDocRef);
+      const userData = userDoc.data();
+  
+      let updatedFriends = [];
+      if (userData.friends && Array.isArray(userData.friends)) {
+        updatedFriends = [...userData.friends, request];
+      } else {
+        updatedFriends = [request];
+      }
+  
+      const updatedRequests = requests.filter(
+        (req) => req.userDocID !== request.userDocID
+      );
+  
+      await updateDoc(userDocRef, {
+        requests: updatedRequests,
+        friends: updatedFriends,
+      });
+  
+      const friendDocRef = doc(db, "users", request.userDocID);
+      const friendDoc = await getDoc(friendDocRef);
+      const friendData = friendDoc.data();
+  
+      let updatedFriendFriends = [];
+      if (friendData.friends && Array.isArray(friendData.friends)) {
+        updatedFriendFriends = [
+          ...friendData.friends,
+          {
+            name: userData.name,
+            userDocID: userDocID,
+          },
+        ];
+      } else {
+        updatedFriendFriends = [
+          {
+            name: userData.name,
+            userDocID: userDocID,
+          },
+        ];
+      }
+  
+      const updatedFriendData = {
+        ...friendData,
+        friends: updatedFriendFriends,
+      };
+  
+      await updateDoc(friendDocRef, updatedFriendData);
+      const updatedUserDoc = await getDoc(userDocRef);
+      const updatedUserData = updatedUserDoc.data();
+  
+      const requestsData = updatedUserData?.requests || [];
+      setRequests(requestsData);
+  
+      const friendsData = updatedUserData?.friends || [];
+      const filteredFriends = [];
+  
+      for (const friend of friendsData) {
+        const friendDocRef = doc(db, "users", friend.userDocID);
+        const friendDoc = await getDoc(friendDocRef);
+        const friendData = friendDoc.data();
+        filteredFriends.push(friendData);
+      }
+      setFriends(filteredFriends);
+    } catch (error) {
+      console.log("Error accepting request:", error);
+    }
+  };
+  
+  
+  const handleRemoveFriend = async (friendId) => {
+    try {
+      const userDocRef = doc(db, "users", userDocID);
+      const userDoc = await getDoc(userDocRef);
+      const userData = userDoc.data();
+  
+      const updatedFriends = userData.friends.filter(
+        (friend) => friend.userDocID !== friendId
+      );
+  
+      await updateDoc(userDocRef, { friends: updatedFriends });
+  
+      const friendDocRef = doc(db, "users", friendId);
+      const friendDoc = await getDoc(friendDocRef);
+      const friendData = friendDoc.data();
+  
+      const updatedFriendFriends = friendData.friends.filter(
+        (friend) => friend.userDocID !== userDocID
+      );
+  
+      await updateDoc(friendDocRef, { friends: updatedFriendFriends });
+      setFriends(updatedFriends);
+    } catch (error) {
+      console.log("Error removing friend:", error);
+    }
+  };
+  return {
+    handleAccept,
+    handleRemoveFriend,
+    initialFocusRef
+  };
+};
+
+export default FriendsLogic;
--- tracker/src/views/Friends/Friends.jsx
+++ tracker/src/views/Friends/Friends.jsx
@@ -1,118 +1,21 @@
 import {Box,Text,Button,ButtonGroup,Flex,Heading,} from "@chakra-ui/react";
-import { AuthContext } from "../../context/AuthContext";
-import { useContext,useRef } from "react";
-import {doc,updateDoc,getDoc,} from "firebase/firestore";
-import { db } from "../../config/firebase";
+import { useContext } from "react";
 import {Popover, Tooltip,Avatar,Td,Th,Tbody,Thead,Table,Tr,Grid,PopoverTrigger,PopoverContent,GridItem,PopoverHeader,PopoverArrow,PopoverCloseButton,PopoverBody,PopoverFooter} from "@chakra-ui/react";
 import goalheader from "../../assets/goal.png"
 import {BsFillPersonXFill,} from "react-icons/bs";
 import { FriendsContext } from "../../context/FriendsContext";
 import "./Friends.css"
 import user2 from "../../assets/user2.png"
+import FriendsLogic from "../../components/FriendsLogic/FriendsLogic";
 
 const Friends = () => {
-  const { userDocID } = useContext(AuthContext);
-  const {requests, setRequests, friends, setFriends} = useContext(FriendsContext)
-  const initialFocusRef = useRef();
+  const {requests, friends} = useContext(FriendsContext)
+  const {
+    handleAccept,
+    handleRemoveFriend,
+    initialFocusRef
+  } = FriendsLogic();
 
-  const handleAccept = async (request) => {
-    try {
-      const userDocRef = doc(db, "users", userDocID);
-      const userDoc = await getDoc(userDocRef);
-      const userData = userDoc.data();
-  
-      let updatedFriends = [];
-      if (userData.friends && Array.isArray(userData.friends)) {
-        updatedFriends = [...userData.friends, request];
-      } else {
-        updatedFriends = [request];
-      }
-  
-      const updatedRequests = requests.filter(
-        (req) => req.userDocID !== request.userDocID
-      );
-  
-      await updateDoc(userDocRef, {
-        requests: updatedRequests,
-        friends: updatedFriends,
-      });
-  
-      const friendDocRef = doc(db, "users", request.userDocID);
-      const friendDoc = await getDoc(friendDocRef);
-      const friendData = friendDoc.data();
-  
-      let updatedFriendFriends = [];
-      if (friendData.friends && Array.isArray(friendData.friends)) {
-        updatedFriendFriends = [
-          ...friendData.friends,
-          {
-            name: userData.name,
-            userDocID: userDocID,
-          },
-        ];
-      } else {
-        updatedFriendFriends = [
-          {
-            name: userData.name,
-            userDocID: userDocID,
-          },
-        ];
-      }
-  
-      const updatedFriendData = {
-        ...friendData,
-        friends: updatedFriendFriends,
-      };
-  
-      await updateDoc(friendDocRef, updatedFriendData);
-      const updatedUserDoc = await getDoc(userDocRef);
-      const updatedUserData = updatedUserDoc.data();
-  
-      const requestsData = updatedUserData?.requests || [];
-      setRequests(requestsData);
-  
-      const friendsData = updatedUserData?.friends || [];
-      const filteredFriends = [];
-  
-      for (const friend of friendsData) {
-        const friendDocRef = doc(db, "users", friend.userDocID);
-        const friendDoc = await getDoc(friendDocRef);
-        const friendData = friendDoc.data();
-        filteredFriends.push(friendData);
-      }
-      setFriends(filteredFriends);
-    } catch (error) {
-      console.log("Error accepting request:", error);
-    }
-  };
-  
-  
-  const handleRemoveFriend = async (friendId) => {
-    try {
-      const userDocRef = doc(db, "users", userDocID);
-      const userDoc = await getDoc(userDocRef);
-      const userData = userDoc.data();
-  
-      const updatedFriends = userData.friends.filter(
-        (friend) => friend.userDocID !== friendId
-      );
-  
-      await updateDoc(userDocRef, { friends: updatedFriends });
-  
-      const friendDocRef = doc(db, "users", friendId);
-      const friendDoc = await getDoc(friendDocRef);
-      const friendData = friendDoc.data();
-  
-      const updatedFriendFriends = friendData.friends.filter(
-        (friend) => friend.userDocID !== userDocID
-      );
-  
-      await updateDoc(friendDocRef, { friends: updatedFriendFriends });
-      setFriends(updatedFriends);
-    } catch (error) {
-      console.log("Error removing friend:", error);
-    }
-  };
   
   
   return (
