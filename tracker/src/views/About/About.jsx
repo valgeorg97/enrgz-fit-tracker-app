9193c2c234c3a0506c9a867b74aeea38af6a11d8
Valentin Georgiev <valentingeorgiev1997@abv.bg>
2023-06-11 01:26:24 +0300

    Add Styling to About sector in LandingPage

--- tracker/src/views/About/About.jsx
+++ tracker/src/views/About/About.jsx
@@ -1,34 +1,60 @@
-import {Box,Heading,Image,Text,Button,Flex,} from '@chakra-ui/react';
+import { Box, Heading, Image, Text, Button, Flex, Icon, Link } from '@chakra-ui/react';
+import { FaLinkedin, FaEnvelope } from 'react-icons/fa';
 import valentinImage from "../../assets/pic.png";
 import samiImage from "../../assets/sami.jpg";
 import logo from "../../assets/logo.png";
 
 const About = () => (
-  <Box textAlign="center" mb={"32"}>
-    <Image src={logo} alt="Team Image" maxW="230px" mx="auto" my={4} borderRadius="md" />
-    <Heading as="h2" fontSize="2xl" my={4}>
-      Creators of ENERGIZE App
-    </Heading>
-    <Flex justify="center">
-      <Box maxW="300px" mx={4} p={4} borderWidth="1px" borderRadius="lg" overflow="hidden">
-        <Image src={samiImage} alt="Samuil" objectFit="cover" h="300px" borderRadius="md" />
-        <Heading as="h3" fontSize="xl" my={2}>Samuil Yoshkov</Heading>
-        <Text fontWeight="bold" mb={4}>
-          Junior JavaScript developer passionate about nature, hiking, and coding.
+  <Box maxWidth="960px" mx="auto" p={6} bg="white" >
+    <Box textAlign="center" mb={"32"}>
+      <Image src={logo} alt="Team Image" maxW="230px" mx="auto" my={4} borderRadius="md" />
+      <Text fontSize="sm" textAlign="center" maxW="600px" mx="auto">
+        <Text fontSize="md" textAlign="center" maxW="600px" mx="auto">
+          Meet us, two Junior JavaScript developers with a shared passion for fitness. Energize isn't just a fitness tracker; it's a platform that helps you manage workouts, track your goals, and even rewards your progress. Our vision? To make Energize your reliable fitness buddy, inspiring and supporting you through every step of your wellness journey. Don't just dream it, achieve it. Unleash your potential. Get Energized!
         </Text>
-        <Text fontSize="sm" mb={2}>samuilmnt@gmail.com</Text>
-        <Button colorScheme="blue">Contact</Button>
-      </Box>
-      <Box maxW="300px" mx={4} p={4} borderWidth="1px" borderRadius="lg" overflow="hidden">
-        <Image src={valentinImage} alt="Valentin" objectFit="cover" h="300px" borderRadius="md" />
-        <Heading as="h3" fontSize="xl" my={2}>Valentin Georgiev</Heading>
-        <Text fontWeight="bold" mb={4}>
-          Junior JavaScript developer with a passion for fitness and sports.
-        </Text>
-        <Text fontSize="sm" mb={2}>valentingg1997@gmail.com</Text>
-        <Button colorScheme="blue">Contact</Button>
-      </Box>
-    </Flex>
+      </Text>
+      <Heading as="h2" fontSize="2xl" my={4}>
+        Creators of ENERGIZE App
+      </Heading>
+      <Flex justify="center" mb={4}>
+        <Box maxW="300px" mx={4} p={4} borderWidth="1px" borderRadius="lg" overflow="hidden">
+          <Image src={samiImage} alt="Samuil" objectFit="cover" h="300px" borderRadius="md" />
+          <Heading as="h3" fontSize="xl" my={2}>Samuil Yoshkov</Heading>
+          <Text fontWeight="bold" mb={4}>
+            Junior JavaScript developer passionate about nature, hiking, and coding.
+          </Text>
+          <Heading as="h4" fontSize="md" my={2}>Contacts:</Heading>
+          <Flex alignItems="center" ml={"24"} mt={5}>
+            <Link href="mailto:example.com" isExternal>
+              <Icon as={FaEnvelope} boxSize={6} mr={2} />
+
+            </Link>
+            <Link href="memberLinkedIn" isExternal ml={4}>
+              <Icon as={FaLinkedin} boxSize={6} mr={2} />
+
+            </Link>
+          </Flex>
+        </Box>
+        <Box maxW="300px" mx={4} p={4} borderWidth="1px" borderRadius="lg" overflow="hidden">
+          <Image src={valentinImage} alt="Valentin" objectFit="cover" h="300px" borderRadius="md" />
+          <Heading as="h3" fontSize="xl" my={2}>Valentin Georgiev</Heading>
+          <Text fontWeight="bold" mb={4}>
+            Junior JavaScript developer with a passion for fitness, sports and hiking.
+          </Text>
+          <Heading as="h4" fontSize="md" my={2}>Contacts:</Heading>
+          <Flex alignItems="center" ml={"24"} mt={5}>
+            <Link href="mailto:valentingg1997@gmail.com" isExternal>
+              <Icon as={FaEnvelope} boxSize={6} mr={2} />
+
+            </Link>
+            <Link href="https://www.linkedin.com/in/valentin-georgiev-414950160/" isExternal ml={4}>
+              <Icon as={FaLinkedin} boxSize={6} mr={2} />
+
+            </Link>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
   </Box>
 );
 
